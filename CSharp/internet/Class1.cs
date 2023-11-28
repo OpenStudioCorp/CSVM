f4a1dad6e17a30130828a84e93e3ab675918bf8d
charlie_sans <gamerboy030607@gmail.com>
2023-11-29 03:27:58 +1100

    upload libs

---
+++ CSharp/internet/Class1.cs
@@ -0,0 +1,141 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+using Newtonsoft.Json.Linq;
+using Newtonsoft.Json.Schema;
+using Newtonsoft.Json.Schema.Generation;
+
+
+namespace internet
+{
+    public class json
+    {
+        public static async Task<string> GetJsonAsync(string url)
+        {
+            using (HttpClient client = new HttpClient())
+            {
+                HttpResponseMessage response = await client.GetAsync(url);
+                response.EnsureSuccessStatusCode();
+                string responseBody = await response.Content.ReadAsStringAsync();
+                return responseBody;
+            }
+        }
+        public static async Task<string> PostJsonAsync(string url, string json)
+        {
+            using (HttpClient client = new HttpClient())
+            {
+                HttpContent content = new StringContent(json, Encoding.UTF8, "application/json");
+                HttpResponseMessage response = await client.PostAsync(url, content);
+                response.EnsureSuccessStatusCode();
+                string responseBody = await response.Content.ReadAsStringAsync();
+                return responseBody;
+            }
+        }   
+        public static async Task<string> PutJsonAsync(string url, string json)
+        {
+            using (HttpClient client = new HttpClient())
+            {
+                HttpContent content = new StringContent(json, Encoding.UTF8, "application/json");
+                HttpResponseMessage response = await client.PutAsync(url, content);
+                response.EnsureSuccessStatusCode();
+                string responseBody = await response.Content.ReadAsStringAsync();
+                return responseBody;
+            }
+        }
+        public static async Task<string> DeleteJsonAsync(string url)
+        {
+            using (HttpClient client = new HttpClient())
+            {
+                HttpResponseMessage response = await client.DeleteAsync(url);
+                response.EnsureSuccessStatusCode();
+                string responseBody = await response.Content.ReadAsStringAsync();
+                return responseBody;
+            }
+        }
+        public static async Task<string> PatchJsonAsync(string url, string json)
+        {
+            using (HttpClient client = new HttpClient())
+            {
+                HttpContent content = new StringContent(json, Encoding.UTF8, "application/json");
+                HttpRequestMessage request = new HttpRequestMessage(new HttpMethod("PATCH"), url)
+                {
+                    Content = content
+                };
+                HttpResponseMessage response = await client.SendAsync(request);
+                response.EnsureSuccessStatusCode();
+                string responseBody = await response.Content.ReadAsStringAsync();
+                return responseBody;
+            }
+        }   
+        public static async Task<string> HeadJsonAsync(string url)
+        {
+            using (HttpClient client = new HttpClient())
+            {
+                HttpResponseMessage response = await client.SendAsync(new HttpRequestMessage(HttpMethod.Head, url));
+                response.EnsureSuccessStatusCode();
+                string responseBody = await response.Content.ReadAsStringAsync();
+                return responseBody;
+            }
+        }
+        public static async Task<string> OptionsJsonAsync(string url)
+        {
+            using (HttpClient client = new HttpClient())
+            {
+                HttpResponseMessage response = await client.SendAsync(new HttpRequestMessage(HttpMethod.Options, url));
+                response.EnsureSuccessStatusCode();
+                string responseBody = await response.Content.ReadAsStringAsync();
+                return responseBody;
+            }
+        }   
+        public static async Task<string> TraceJsonAsync(string url)
+        {
+            using (HttpClient client = new HttpClient())
+            {
+                HttpResponseMessage response = await client.SendAsync(new HttpRequestMessage(HttpMethod.Trace, url));
+                response.EnsureSuccessStatusCode();
+                string responseBody = await response.Content.ReadAsStringAsync();
+                return responseBody;
+            }
+        }   
+        
+            public static async Task<string> GetJsonAsync(string url, string username, string password)
+            {
+            using (HttpClient client = new HttpClient())
+            {
+                client.DefaultRequestHeaders.Authorization = new System.Net.Http.Headers.AuthenticationHeaderValue("Basic", Convert.ToBase64String(Encoding.ASCII.GetBytes($"{username}:{password}")));
+                HttpResponseMessage response = await client.GetAsync(url);
+                response.EnsureSuccessStatusCode();
+                string responseBody = await response.Content.ReadAsStringAsync();
+                return responseBody;
+            }
+        }   
+
+        // method to read json from file
+
+        public static string ReadJsonFromFile(string path)
+        {
+            string json = "";
+            using (StreamReader r = new StreamReader(path))
+            {
+                json = r.ReadToEnd();
+            }
+            return json;
+        }
+
+        // method to write json to file
+
+        public static void WriteJsonToFile(string path, string json)
+        {
+            using (StreamWriter w = new StreamWriter(path))
+            {
+                w.Write(json);
+            }
+        }
+        
+        
+    }
+}
---
+++ Kotlin/home/src/main/java/org/example/Main.java
@@ -0,0 +1,7 @@
+package org.example;
+import home.Home.*;
+public class Main {
+    public static void main(String[] args) {
+    print("Hello World!");
+    }
+}
